3ffff613832f431cc1de88e7476d49cb5483cfc3
vijayshah263 <shahvijayrajendra02@gmail.com>
2025-11-17 02:05:09 +0530

    dataTypes and oper

---
+++ 02_dataTypes.js
@@ -0,0 +1,6 @@
+//primiitive - string, number, boolean, null, undefined, bigint, symbol
+//reference - function, array, objects 
+              //[], {}, ()
+
+let a = null // null value janmujkar deta he, kyoki bad me usea change kar sake..
+let b; // udefined- value detch nhi hen
---
+++ 03_operatores.js
@@ -0,0 +1,12 @@
+//arithematic +,-. /, *, %, **
+//comparison =, ==, ===, !=, !==, >=, <=, >, <
+//logical 
+//  assignement =
+//  unary ++, --
+// tarnary ?:
+// 12<13 ? console.log("true") : console.log("false");
+
+//imp
+let i = 1;
+console.log(++i);
+console.log(i++);
