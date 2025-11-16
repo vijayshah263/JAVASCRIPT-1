33848322f4078664ec2851a4ac3e6dda00b27871
vijayshah263 <shahvijayrajendra02@gmail.com>
2025-11-17 02:01:17 +0530

    variables

--- 01_variables.js
+++ 01_variables.js
@@ -1,1 +1,69 @@
-fdg
+//var, let, const 
+// var a = 12;
+// let a =12;
+// const a = 12;
+// a =12;
+// let a;
+//const a; //error
+
+
+//block, global, functional
+// {
+//     let a =12;
+// }
+// console.log(a);
+
+
+function abc() {
+    if (true) {
+         var a = 12;
+    }
+    console.log(a);
+}
+
+
+abc()
+
+
+// const a = 12;
+
+// {
+//     console.log(a);
+    
+// }
+
+
+
+// var a =2;
+// var a= 3
+// // console.log(a)
+
+// let b =2;
+//  b= 3
+// console.log(b);
+
+// const c = 3;
+// c = 4
+// console.log(c);
+
+
+//Temporal deadzone
+// console.log(a);
+
+// let a = 12
+
+
+//hoisting
+// var a = undefined;
+// console.log(a);
+// var a= 12
+
+
+//Qs
+// let a = 10;
+// {
+//     let a =20;
+//     console.log(a);
+    
+// }
+// console.log(a);
